e02735d50009cb2282e03ed0acabc1d943599ba1
Alvis F <falvis1999@gmail.com>
2020-08-21 16:21:35 +0530

    🔥created a todo list

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,39 @@
 <template>
-  <div>
-    <h1>TodoList</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">TactLabs</div>
+    </div>
+    <div class="row">
+      <NewTodo @on-addTodo="addTodo($event)" />
+    </div>
+    <div class="row">
+      <p>Add create form</p>
+    </div>
+    <div class="row">
+      <div class="col-12 col-lg-6">
+        <ul class="list-group">
+          <Todo
+            v-for="(todo, index) in todos"
+            :key="index"
+            :todoString="todo.todoString"
+            :completed="todo.completed"
+            @on-delete="deleteTodo(todo)"
+            @on-toggle="toggleTodo(todo)"
+            @on-edit="editTodo(todo,$event)"
+          />
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import Todo from "./Todo";
+import NewTodo from "./NewTodo";
 export default {
+  components: {
+    Todo,
+    NewTodo,
+  },
   data() {
     return {
       todos: [
@@ -15,10 +44,29 @@ export default {
       ],
     };
   },
+  methods: {
+    addTodo(newTodo) {
+      this.todos.push({
+        todoString: newTodo,
+        completed: false,
+      });
+    },
+    toggleTodo(todo) {
+      todo.completed = !todo.completed;
+    },
+    editTodo(todo, newTodoString) {
+      todo.todoString = newTodoString;
+    },
+    deleteTodo(deleteTodo) {
+      this.todos = this.todos.filter(
+        (todo) => todo.todoString != deleteTodo.todoString
+      );
+    },
+  },
 };
 </script>
 <style scoped>
-h1 {
+div {
   color: #fff;
 }
 </style>
